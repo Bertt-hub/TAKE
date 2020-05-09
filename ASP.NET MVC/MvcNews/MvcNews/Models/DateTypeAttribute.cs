22e2b7479690dec54994614ceb1987fa71a32569
Bertt-hub <58176673+Bertt-hub@users.noreply.github.com>
2020-05-09 11:08:39 +0200

    Lab 8

---
+++ ASP.NET MVC/MvcNews/MvcNews/Models/DateTypeAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MvcNews.Models
+{
+    internal class DateTypeAttribute : Attribute
+    {
+    }
+}
---
+++ ASP.NET MVC/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+
+        [Required]
+        [StringLength(140, MinimumLength = 5)]
+        public string Text { get; set; }
+
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
+    }
+}
